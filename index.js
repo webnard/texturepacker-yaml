522caac81fe12a6ed9ca9e007ed6251ebc743a47
Ian Hunter <beanland2@gmail.com>
2016-04-05 21:26:25 -0400

    Hitbox support, changes frames to zero-index and restructures data.

--- index.js
+++ index.js
@@ -10,12 +10,12 @@ module.exports = {
 
     var newData = {
      "meta": data['meta'],
-     "frames": {}
+     "sprites": {}
     };
 
     Object.keys(data.frames).forEach(function(filename, kindex) {
       var shortfile = filename.substr(0, filename.lastIndexOf('.'));
-      var item = newData.frames;
+      var item = newData.sprites;
       var meta;
       var parts = shortfile.split('-');
       var hasNumber = !Number.isNaN(parseInt(parts[parts.length - 1], 10));
@@ -23,16 +23,16 @@ module.exports = {
 
       shortfile.split('-').forEach(function(part, idx, arr) {
         if(idx === arr.length-2 && hasNumber) {
-          item[part] = item[part] || [];
+          item[part] = item[part] || {"frames": [], hitbox: {w: 0, h: 0}};
         }
         else if(idx === arr.length-1) {
           if(!hasNumber) {
-            item[part] = item[part] || [];
-            meta = {index: item[part].length, filename: filename};
+            item[part] = item[part] || {"frames": [], hitbox: {w: 0, h: 0}};
+            meta = {index: item[part].length || 0, filename: filename};
           }
           else
           {
-            meta = {index: number, filename: filename};
+            meta = {index: number-1, filename: filename};
             return;
           }
         }
@@ -42,7 +42,9 @@ module.exports = {
         item = item[part];
       });
       extend(true, meta, data.frames[filename]);
-      item.push(meta);
+      item.frames.push(meta);
+      item.hitbox.w = data.frames[filename].sourceSize.w;
+      item.hitbox.h = data.frames[filename].sourceSize.h;
     });
     return json2yaml.stringify(newData);
   }
--- test/test.sh
+++ test/test.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+echo "Checking conversion of test/sprites.json to test/sprites.yml"
 ./cli.js test/sprites.json | diff test/sprites.yml -
+
+echo "Checking conversion of test/sprites2.json to test/sprites2.yml"
 ./cli.js test/sprites2.json | diff test/sprites2.yml -
